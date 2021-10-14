69423820a7b545706199f9632748a60de6febf6e
Victor <victor.f.ramos@outlook.com>
2021-10-14 12:19:29 -0300

    funcionalidade créditos adicionada

--- script.js
+++ script.js
@@ -2,19 +2,27 @@ let display = document.querySelector('.display');
 let resultado = document.querySelector('.resultado');
 let numeros = document.querySelector('.numeros')
 let numero = document.querySelectorAll('.numero')
+let credito = document.querySelector('.credito')
 
 let numeroEscolhido = null;
+let creditos = 0;
+
+credito.innerText = creditos
 
 for(let i=0;i<numero.length;i++){
     numero[i].addEventListener('click', (e)=>selecionar(e))
 }
 
 const sortear = () => {
+    creditos--;
+    credito.innerText = creditos
     if(numeroEscolhido !== null){
         let numeroSorteado = Math.round((Math.random()*9)+1);
         display.innerText = numeroSorteado;
         if (numeroSorteado == numeroEscolhido){
-            resultado.innerText = 'Parabéns! Você ganhou.';
+            creditos = creditos + 10;
+            credito.innerText = creditos
+            resultado.innerText = 'Parabéns! Você ganhou 10 créditos.';
         } else {
             resultado.innerText = 'Não foi dessa vez. Tente novamente!';
         }
@@ -34,6 +42,8 @@ const resetar = () => {
     display.innerText = '$';
     resultado.innerText = 'Escolha um número e clique em Sortear para começar';
     numeroEscolhido = null;
+    creditos = 0;
+    credito.innerText = creditos
 }
 
 const selecionar = e => {
@@ -45,4 +55,9 @@ const selecionar = e => {
 
     numeroEscolhido = e.target.value;
     e.target.classList.add('selecionado')
+}
+
+const inserirCreditos = () => {
+    creditos = creditos + 10;
+    credito.innerText = creditos
 }
