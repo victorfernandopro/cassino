15f5b1c22f18a38b6627dc560150f5aab0c90a32
Victor <victor.f.ramos@outlook.com>
2021-10-14 14:35:29 -0300

    adicionado controle básico sobre créditos

--- script.js
+++ script.js
@@ -9,45 +9,52 @@ let creditos = 0;
 
 credito.innerText = creditos
 
-for(let i=0;i<numero.length;i++){
-    numero[i].addEventListener('click', (e)=>selecionar(e))
+for (let i = 0; i < numero.length; i++) {
+    numero[i].addEventListener('click', (e) => selecionar(e))
 }
 
 const sortear = () => {
-    creditos--;
-    credito.innerText = creditos
-    if(numeroEscolhido !== null){
-        let numeroSorteado = Math.round((Math.random()*9)+1);
-        display.innerText = numeroSorteado;
-        if (numeroSorteado == numeroEscolhido){
-            creditos = creditos + 10;
+    if (creditos > 0) {
+        if (numeroEscolhido !== null) {
+            creditos--;
             credito.innerText = creditos
-            resultado.innerText = 'Parabéns! Você ganhou 10 créditos.';
+            let numeroSorteado = Math.round((Math.random() * 9) + 1);
+            display.innerText = numeroSorteado;
+            if (numeroSorteado == numeroEscolhido) {
+                creditos = creditos + 10;
+                credito.innerText = creditos
+                resultado.innerText = 'Parabéns! Você ganhou 10 créditos.';
+            } else {
+                resultado.innerText = 'Não foi dessa vez. Tente novamente!';
+            }
         } else {
-            resultado.innerText = 'Não foi dessa vez. Tente novamente!';
+            resultado.innerText = 'Selecione um número';
+            setTimeout(() => {
+                resultado.innerText = 'Escolha um número e clique em Sortear para começar';
+            }, 1200)
         }
     } else {
-        resultado.innerText = 'Nenhum número foi selecionado';
-        setTimeout(()=>{
-            resultado.innerText = 'Escolha um número e clique em Sortear para começar';
-        },1200)
+        resultado.innerText = 'Insira créditos para começar';
+        setTimeout(() => {
+            resultado.innerText = 'Insira créditos, escolha um número e clique em Sortear para começar';
+        }, 1200)
     }
 }
 
 const resetar = () => {
-    for(let i=0;i<numero.length;i++){
+    for (let i = 0; i < numero.length; i++) {
         numero[i].classList.remove('selecionado');
     }
-    
+
     display.innerText = '$';
-    resultado.innerText = 'Escolha um número e clique em Sortear para começar';
+    resultado.innerText = 'Insira créditos, escolha um número e clique em Sortear para começar';
     numeroEscolhido = null;
     creditos = 0;
     credito.innerText = creditos
 }
 
 const selecionar = e => {
-    for(let i=0;i<numero.length;i++){
+    for (let i = 0; i < numero.length; i++) {
         numero[i].classList.remove('selecionado');
     }
     display.innerText = '$';
