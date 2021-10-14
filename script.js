a78f89fb4e0689445a1c0b582747d031b58f5e59
Victor <victor.f.ramos@outlook.com>
2021-10-14 11:44:28 -0300

    Atualizações das funcionalidades e estilo

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 let display = document.querySelector('.display');
 let resultado = document.querySelector('.resultado');
 let numeros = document.querySelector('.numeros')
+let numero = document.querySelectorAll('.numero')
+
 let numeroEscolhido = null;
 
-numeros.addEventListener('click', (e)=>numeroEscolhido = e.target.value)
+for(let i=0;i<numero.length;i++){
+    numero[i].addEventListener('click', (e)=>selecionar(e))
+}
 
 const sortear = () => {
-    let numeroSorteado = Math.round((Math.random()*9)+1);
-    display.innerText = numeroSorteado;
-    if (numeroSorteado == numeroEscolhido){
-        resultado.innerText = 'Parabéns! Você ganhou.'
+    if(numeroEscolhido !== null){
+        let numeroSorteado = Math.round((Math.random()*9)+1);
+        display.innerText = numeroSorteado;
+        if (numeroSorteado == numeroEscolhido){
+            resultado.innerText = 'Parabéns! Você ganhou.';
+        } else {
+            resultado.innerText = 'Não foi dessa vez. Tente novamente!';
+        }
     } else {
-        resultado.innerText = 'Não foi dessa vez. Tente novamente!'
+        resultado.innerText = 'Nenhum número foi selecionado';
+        setTimeout(()=>{
+            resultado.innerText = 'Escolha um número e clique em Sortear para começar';
+        },1200)
     }
 }
 
 const resetar = () => {
-    display.innerText = '$'
-    resultado.innerText = 'Clique em Sortear para começar'
+    for(let i=0;i<numero.length;i++){
+        numero[i].classList.remove('selecionado');
+    }
+    
+    display.innerText = '$';
+    resultado.innerText = 'Escolha um número e clique em Sortear para começar';
+    numeroEscolhido = null;
+}
+
+const selecionar = e => {
+    for(let i=0;i<numero.length;i++){
+        numero[i].classList.remove('selecionado');
+    }
+    display.innerText = '$';
+    resultado.innerText = 'Escolha um número e clique em Sortear para começar';
+
+    numeroEscolhido = e.target.value;
+    e.target.classList.add('selecionado')
 }
